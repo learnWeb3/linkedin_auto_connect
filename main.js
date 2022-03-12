3e8800685a75e8a6f441665cb38a76d2ba7cc7b0
Antoine <antoinehadrienleguillou@outlook.com>
2022-03-12 14:59:43 +0100

    udpate code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { writeFileSync } = require("fs");
+const { writeFileSync, readFileSync, read, readFile, fstat } = require("fs");
 const { join } = require("path");
 const Base = require("./src/Base");
 
@@ -12,42 +12,83 @@ const pause = (pauseLengthMs) =>
 const run = async () => {
   const BASE_URL = "https://www.annuaire-des-mairies.com/";
   const SCRAPPER = new Base("www.annuaire-des-mairies.com", BASE_URL);
-  await SCRAPPER.navigate();
-  const departements = await SCRAPPER.extractFromPage(
-    ".table.table-border.table-mobile.mobile-primary.bg-white a.lientxt"
-  );
-  const departementWithTownShips = [];
-  for (let i = 0; i < departements.length; i++) {
-    //for (let i = 0; i < 1; i++) {
-    const departement = departements[i];
-    SCRAPPER.url = BASE_URL + departement.href;
-    await SCRAPPER.navigate();
-    let townships = await SCRAPPER.extractFromPage("a.lientxt");
-    await pause(500);
 
-    for (let page = 1; page < 10; page++) {
-      SCRAPPER.url =
-        BASE_URL + `${departement.href.replace(".html", "")}-${page}.html`;
 
-      await SCRAPPER.navigate();
-      const _townships = await SCRAPPER.extractFromPage("a.lientxt");
-      if (_townships.length === 0 && page > 1) {
-        break;
-      } else {
-        townships = [...townships, ..._townships];
-      }
-    }
-
-    departementWithTownShips.push({
-      ...departement,
-      townships,
-    });
+  // check if scrapper has already been launched on a previous session
+  const fileName  = 'departements_and_townships_links'
+  const fileFullName = `${fileName}.json`;
+  const temp_data_file_path = join(process.cwd(), 'temp', fileFullName)
+  let departementWithTownShips = null;
+  try {
+    departementWithTownShips = readFileSync(temp_data_file_path, {encoding: 'utf-8'});
+    departementWithTownShips = JSON.parse(departementWithTownShips)
+  } catch (error) {
+    console.log(`temp data does not exists yet`)
   }
 
-  const departementWithTownShipsDetails = [];
+  if(!departementWithTownShips){
+    await SCRAPPER.navigate();
+    const departements = await SCRAPPER.extractFromPage(
+      ".table.table-border.table-mobile.mobile-primary.bg-white a.lientxt"
+    );
+    let departementWithTownShips = [];
+    for (let i = 0; i < departements.length; i++) {
+      //for (let i = 0; i < 1; i++) {
+      const departement = departements[i];
+      SCRAPPER.url = BASE_URL + departement.href;
+      await SCRAPPER.navigate();
+      let townships = await SCRAPPER.extractFromPage("a.lientxt");
+      await pause(500);
+  
+      for (let page = 1; page < 10; page++) {
+        SCRAPPER.url =
+          BASE_URL + `${departement.href.replace(".html", "")}-${page}.html`;
+  
+        await SCRAPPER.navigate();
+        const _townships = await SCRAPPER.extractFromPage("a.lientxt");
+        if (_townships.length === 0 && page > 1) {
+          break;
+        } else {
+          townships = [...townships, ..._townships];
+        }
+      }
+  
+      departementWithTownShips.push({
+        ...departement,
+        townships,
+      });
+    }
+    
+    try {
+      writeFileSync(temp_data_file_path,  JSON.stringify(departementWithTownShips));
+      departementWithTownShips = readFileSync(temp_data_file_path, {encoding: 'utf-8'});
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
-  for (let i = 0; i < departementWithTownShips.length; i++) {
+
+  const departementWithTownShipsDetails = [];
+  let currentDepartment = null
+  try {
+    const currentDepartmentPath = join(process.cwd(), 'temp', 'current_departement.json')
+    currentDepartment = JSON.parse(readFileSync(currentDepartmentPath, {encoding: 'utf-8'}))
+  } catch (error) {
+    console.log(error)
+    currentDepartment = null
+  }
+
+  let departementIndex = currentDepartment? currentDepartment.index : 0;
+
+  for (let i = departementIndex; i < departementWithTownShips.length; i++) {
+
     const departement = departementWithTownShips[i];
+
+    const path = join(process.cwd(), 'temp', 'current_departement.json')
+    writeFileSync(path, JSON.stringify({...departement, index: i}), {
+      encoding: 'utf-8'
+    });
+
     const townshipsWithDetails = [];
     for (let j = 0; j < departement.townships.length; j++) {
       try {
@@ -97,19 +138,23 @@ const run = async () => {
         });
       } catch (error) {
         console.error(departement.townships[j]);
+      }finally{
         continue;
       }
     }
-  }
 
+    const fileName = departementWithTownShipsDetails[i].href.replace('.html', '')
+    const fileFullName = `${fileName}.json`;
+    try {
+      writeFileSync(join(process.cwd(), 'data', fileFullName),  JSON.stringify(departementWithTownShipsDetails[i]))
+    } catch (error) {
+      console.log(error)
+    }
+   
+  
+  }
   return departementWithTownShipsDetails;
 };
 
 run()
-  .then((data) => {
-    for(let i=0; i<data.length; i++){
-      const fileName = `${data[i].text}.json`;
-      writeFileSync(join(process.cwd(), 'data', fileName),  JSON.stringify(data[i]))
-    }
-  })
-.then(() => process.exit(0));
+.then(() => process.exit(0))
